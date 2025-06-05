1b7be9fa83bb7506d521beb0f45e3994660b8bb2
Keita Furumatsu <saba.ad1357@gmail.com>
2025-06-05 09:22:13 +0900

    docs: Update example.php to show usage with secret parameter
    - Demonstrate how to instantiate Subscriber with the new secret argument for hub.secret support
    - Reflect latest API changes in the usage example

--- example.php
+++ example.php
@@ -3,6 +3,7 @@
 // simple example for the PHP pubsubhubbub Subscriber
 // as defined at http://code.google.com/p/pubsubhubbub/
 // written by Josh Fraser | joshfraser.com | josh@eventvue.com
+// written by CureSabs | saba.ad1357@gmail.com
 // Released under Apache License 2.0
 
 include("./src/Subscriber.php");
@@ -11,11 +12,11 @@ use \Pubsubhubbub\Subscriber\Subscriber;
 
 $hub_url = "http://pubsubhubbub.appspot.com";
 $callback_url = "put your own endpoint here";
-
 $feed = "http://feeds.feedburner.com/onlineaspect";
+$secret = "your-very-random-string";
 
-// create a new subscriber
-$s = new Subscriber($hub_url, $callback_url);
+// create a new subscriber with secret
+$s = new Subscriber($hub_url, $callback_url, false, $secret);
 
 // subscribe to a feed
 $s->subscribe($feed);
